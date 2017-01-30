3d53c88e4cc6562c402a6cda700db92ba84e77c8
José Gutiérrez <guti7@users.noreply.github.com>
2017-01-29 18:33:16 -0800

    modify exercise 17 to be more concise.

--- ex17.py
+++ ex17.py
@@ -7,23 +7,28 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
-
-# We could do these on one line
-in_file = open(from_file)
-indata = in_file.read()
+# print "Copying from %s to %s" % (from_file, to_file)
+#
+# # We could do these on one line
+# # in_file = open(from_file)
+# # indata = in_file.read()
 # indata = open(from_file).read()
+#
+# #print "The input file is %d bytes long" % len(indata)
+#
+# #print "Does the output file exist? %r" % exists(to_file)
+# #print "Ready, hit RETURN to continue, CTRL-C to abort."
+# # raw_input()
+#
+# out_file = open(to_file, "w")
+# out_file.write(indata)
+#
+# print "Alright, all done."
+#
+# out_file.close()
+# #in_file.close()
 
-print "The input file is %d bytes long" % len(indata)
-
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
-
-out_file = open(to_file, "w")
-out_file.write(indata)
-
-print "Alright, all done."
-
-out_file.close()
-in_file.close()
+# one line long
+open(to_file, 'w').write(open(from_file).read())
+# When you reach the end of script. It should alaready be closed by Python
+# no need to call .close() on a file
