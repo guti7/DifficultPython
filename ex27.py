efe2ad14a879e5f29a62626f2cf3cf425a3d8ac3
José Gutiérrez <guti7@users.noreply.github.com>
2017-02-02 21:22:40 -0800

    create truth tables for fundamental boolean logic.

---
+++ ex27.py
@@ -0,0 +1,43 @@
+# Exercise 27: Memorizing Logic
+
+# Truth Tables
+
+# NOT
+# not False => True
+# not True => False
+
+# OR
+# True or False => True
+# True or True => True
+# False or True => True
+# False or False => False
+
+# AND
+# True and True   => True
+# True and False  => False
+# False and False => False
+# False and True  => False
+
+# NOT OR
+# not(True or True)   => False
+# not(True or False)  => False
+# not(False or True)  => False
+# not(False or False) => True
+
+# NOT AND
+# not(True and False)  => True
+# not(True and True)   => False
+# not(False and True)  => True
+# not(False and False) => True
+
+# !=
+# 0 != 0 => False
+# 1 != 0 => True
+# 0 != 1 => True
+# 1 != 1 => False
+
+# ==
+# 1 == 0 => False
+# 1 == 1 => True
+# 0 == 1 => False
+# 0 == 0 => True
